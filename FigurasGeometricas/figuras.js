51d910e8dc4fa57a8b0355390467c1a2e21d914b
Javier Bustos Vélez <xaxiredct@gmail.com>
2021-09-07 10:20:55 -0500

    Agregado el formulario y las funciones para calcular el área del cuadrado

--- FigurasGeometricas/figuras.js
+++ FigurasGeometricas/figuras.js
@@ -1,1 +1,59 @@
-console.log("Hola mundo");
+// Código del cuadrado
+console.group("Cuadrado");
+
+function PerimetroCuadrado(lado) {
+    
+    return lado * 4
+};
+
+function AreaCuadrado(lado) {
+    return lado * lado    
+}
+
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triángulos");
+
+function PerimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+
+function AreaTriangulo(base, altura) {
+    return (base * altura) / 2;    
+};
+
+console.groupEnd(); 
+
+// Código del Círculo
+console.group("Círculo");
+
+const Pi = Math.PI;
+
+function DiametroCirculo(radio) {
+    return radio * 2;
+}
+
+function PerimetroCirculo(radio) {
+    const DiametroCirculo = DiametroCirculo(radio);
+    return (DiametroCirculo) * Pi;
+}
+
+function AreaCirculo(radio) {
+    return (radio * radio) * Pi
+}
+
+console.groupEnd();
+
+// Funciones formulario Cuadrado
+function CalcularPerimetroCuadrado() {
+    const InputArea = document.getElementById("txtAreaCuadrado")
+
+    alert(PerimetroCuadrado(InputArea.value))    
+    
+}
+function CalcularAreaCuadrado() {
+    const InputArea = document.getElementById("txtAreaCuadrado")
+
+    alert(AreaCuadrado(InputArea.value))
+}
